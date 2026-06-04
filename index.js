a3a3866d0e25851b60f0dc9e672a7b8799db818f
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-04 16:28:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World,  Shiv!')
+  res.send('Hello World!- Thi is is a GET request')
 })
-
-app.get('/about', (req, res) => {
-  res.send('I am the about page')
+app.post('/', (req, res) => {
+  res.send('Hello World!- This is a POST request')
 })
+app.put('/', (req, res) => {
+  res.send('Hello World!- This is a PUT request')
+})
+        app.delete('/', (req, res) => {
+        res.send('Hello World!- This is a DELETE request')
+        })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port  http://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
+
+        
